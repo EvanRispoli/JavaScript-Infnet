55488a939ad876ba2fc2080aaa957a09f2323ac6
Evan Rispoli <evan.rispoli@gmail.com>
2022-03-11 13:54:39 -0300

    Aula do dia 10/03/2022

---
+++ Anuncio/js/script1.js
@@ -0,0 +1,37 @@
+/*
+
+  PROBLEMAS 10/03/22:
+  1) Observe o objeto 'produto' abaixo no código JS. Adicione um método ao objeto
+     para preencher o HTML de acordo com as seguintes propriedades e elementos 
+     contidos na <div> da <section class="itens">: 
+       - Propriedade 'image': deve completar o atributo src de <img>; 
+       - Propriedade 'title': conteúdo da <div class="title">;
+       - Propriedade 'price': conteúdo da <div class="price">;
+
+  2) Adicione o evento ao botão 'Adicionar' para que quando ele for clicado, 
+     o valor contido na cesta dada pelo <span> contido em <section class="bag">
+     seja atualizado para o preço do objeto produto.
+ */
+
+(() => {
+
+  // Objeto produto.
+  let produto = {
+    "id": 1,
+    "title": "Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  };
+
+  // Acessando propriedades do objeto.
+
+  // Criando o método para preenchimento do HTML.
+
+  // Acionando o método do objeto
+
+  // Criando o método para adicionar à cesta  
+
+  // Adicionar o evento ao botão
+  
+})();
