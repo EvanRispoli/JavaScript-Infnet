3bea68b91ddeab967524fe48443bc1f1adde9494
Evan Rispoli <evan.rispoli@gmail.com>
2022-03-22 11:11:32 -0300

    Adicionar o evento de clique para preencher o contrato.

--- Contrato/js/script.js
+++ Contrato/js/script.js
@@ -26,94 +26,109 @@
     - Use o método AppContratos.criarCartao() para inserir cartões no painel
  */
 
-(() => {
+    (() => {
 
-  let AppContratos = {
-
-    partes: {
-      contratada: null,
-      contratante: null
-    },
-
-    toolbar: {
-      btnContratada: document.querySelector("#btn-contratada"),
-    },
-
-    painel: {
-      principal: document.querySelector("#painel"),
-      cartoes: document.querySelector("#painel .pessoas"),
-      btnFechar: document.querySelector("#painel .p-toolbar button"),
-    },
+      let AppContratos = {
     
-    abrirPainel: function () {
-      this.painel.principal.style.left = 0;
-    },
-
-    fecharPainel: function () {
-      this.painel.principal.style.left = "-1000%";
-    },
-
-    iniciar: function () {
-
-      // Abrir o painel de usuários
-      this.toolbar.btnContratada.addEventListener("click", () => {
-        AppContratos.abrirPainel();
-      });
-      
-      // Botão fechar do painel
-      this.painel.btnFechar.addEventListener("click", () => {
-        AppContratos.fecharPainel();
-      });
-
-      // Invocar a busca de usuários
-      this.buscarUsuarios();
-    },
-
-    buscarUsuarios: function () {
-      // Implementar a busca via API
-
-      // Criar o objeto XMLHttpRequest
-      let rHttp = new XMLHttpRequest();
-
-      rHttp.open("GET", "https://fakestoreapi.com/users");
-      rHttp.addEventListener("load", function () {
-        const listaUsuarios = JSON.parse(this.responseText);
-        // console.log(listaUsuarios);
-
-        // Iterar na lista de usuários
-        for (const usuario of listaUsuarios) {
-          const nome = usuario.name.firstname;
-          const sobrenome = usuario.name.lastname;
-          const email = usuario.email;
-          const cidade = usuario.address.city;
-
-          // Retorna a div com o cartão de usuário
-          let cartao = AppContratos.criarCartao(nome, sobrenome, email, cidade);
-
-          // Adicionar na árvore DOM
-          AppContratos.painel.cartoes.appendChild(cartao);
+        partes: {
+          contratada: null,
+          contratante: null
+        },
+    
+        toolbar: {
+          btnContratada: document.querySelector("#btn-contratada"),
+        },
+    
+        painel: {
+          principal: document.querySelector("#painel"),
+          cartoes: document.querySelector("#painel .pessoas"),
+          btnFechar: document.querySelector("#painel .p-toolbar button"),
+        },
+        
+        abrirPainel: function () {
+          this.painel.principal.style.left = 0;
+        },
+    
+        fecharPainel: function () {
+          this.painel.principal.style.left = "-1000%";
+        },
+    
+        iniciar: function () {
+    
+          // Abrir o painel de usuários
+          this.toolbar.btnContratada.addEventListener("click", () => {
+            AppContratos.abrirPainel();
+          });
+          
+          // Botão fechar do painel
+          this.painel.btnFechar.addEventListener("click", () => {
+            AppContratos.fecharPainel();
+          });
+    
+          // Invocar a busca de usuários
+          this.buscarUsuarios();
+        },
+    
+        buscarUsuarios: function () {
+          // Implementar a busca via API
+    
+          // Criar o objeto XMLHttpRequest
+          let rHttp = new XMLHttpRequest();
+    
+          rHttp.open("GET", "https://fakestoreapi.com/users");
+          rHttp.addEventListener("load", function () {
+            const listaUsuarios = JSON.parse(this.responseText);
+            // console.log(listaUsuarios);
+    
+            // Iterar na lista de usuários
+            for (const usuario of listaUsuarios) {
+              const nome = usuario.name.firstname;
+              const sobrenome = usuario.name.lastname;
+              const email = usuario.email;
+              const cidade = usuario.address.city;
+    
+              // Retorna a div com o cartão de usuário
+              let cartao = AppContratos.criarCartao(nome, sobrenome, email, cidade);
+    
+              // Adicionar na árvore DOM
+              AppContratos.painel.cartoes.appendChild(cartao);
+            }
+          });
+    
+          rHttp.send();
+        },
+    
+        criarCartao: function (nome, sobrenome, email, cidade) {
+          let divPr    = document.createElement("div"),
+              divNome  = document.createElement("div"),
+              divEmail = document.createElement("div");
+    
+          divNome.innerText  = sobrenome;
+          divEmail.innerText = email;
+    
+          divPr.appendChild(divNome);
+          divPr.appendChild(divEmail);
+    
+          // Adicionar o evento de clique para preencher o contrato.
+          // Acessar o elemento span dentro do <p id="contratada"/>
+          divPr.addEventListener("click", () => {
+            let spans = document.querySelectorAll("#contratada span");
+            // Nome completo
+            spans[0].innerText = nome + " " + sobrenome;
+            // Email
+            spans[1].innerText = email;
+            // Cidade
+            spans[2].innerText = cidade;
+    
+            // Fechar o painel
+            AppContratos.fecharPainel();
+          })
+          
+          return divPr;
         }
-      });
-
-      rHttp.send();
-    },
-
-    criarCartao: function (nome, sobrenome, email, cidade) {
-      let divPr    = document.createElement("div"),
-          divNome  = document.createElement("div"),
-          divEmail = document.createElement("div");
-
-      divNome.innerText  = sobrenome;
-      divEmail.innerText = email;
-
-      divPr.appendChild(divNome);
-      divPr.appendChild(divEmail);
-
-      return divPr;
-    }
+        
+      };
+      
+      AppContratos.iniciar();
     
-  };
-  
-  AppContratos.iniciar();
-
-})();
+    })();
