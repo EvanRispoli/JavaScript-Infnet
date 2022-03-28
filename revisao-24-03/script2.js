0937e53ab2570999869b357e31d79fcc1067dec9
Evan Rispoli <evan.rispoli@gmail.com>
2022-03-28 12:19:11 -0300

    Revisão de manipulãção do DOM

---
+++ revisao-24-03/script2.js
@@ -0,0 +1,17 @@
+
+/*
+
+   03) Crie uma aplicação que gere uma lista de itens de 
+   supermercado. O HTML deve conter uma caixa de texto de número
+   com valor inicial igual a 50, mais um botão chamado "gerar lista".
+   Quando o botão for acionado, deve ser gerada uma lista de itens
+   com valores aleatórios entre R$30,00 e R$100,00. Essa lista 
+   deve ser impressa no HTML, gerando elementos dinamicamente.
+
+ */
+
+(() => {
+
+
+
+})();
